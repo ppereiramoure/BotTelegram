f2c7714b3c6932ea79841faa069b5ad61a57da36
Pablo <ppereiramoure@danielcastelao.org>
2021-05-17 14:02:54 +0200

    JavaDoc acabado

--- src/main/kotlin/Echo/main.kt
+++ src/main/kotlin/Echo/main.kt
@@ -6,21 +6,31 @@ import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.network.fold
 
 fun main() {
+
+    /**Instanciamos un objeto bot*/
     val bot = bot {
+
+        /**Introducimos el token del bot*/
         token = Apikey.miToken
+
+        /**El dispatch nos permite enviar información a Telegram para manejar el bot*/
         dispatch {
+
             text {
+
                 print("Chat ID: ")
                 println(message.chat.id)
                 bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = text)
             }
+
+            /**Creamos un comando que devolvera el mensaje que indiquemos*/
             command("start") {
 
                 val result = bot.sendMessage(chatId = ChatId.fromId(update.message!!.chat.id), text = "Bot sovietico iniciado")
 
                 result.fold(
                     {
-                        // do something here with the response
+
                     },
                     {
                         // do something with the error
